8a9b635bd5afb21005e54cc2d6ee671f34a57e46
dsanimm <dsanimm@gmail.com>
2020-09-28 18:25:01 +0530

    TC1.1_TC1.2_repeat

--- JavaPractice/src/main/java/com/JavaPractice/MoodAnalyzer.java
+++ JavaPractice/src/main/java/com/JavaPractice/MoodAnalyzer.java
@@ -1,14 +1,19 @@
 package com.JavaPractice;
 
 public class MoodAnalyzer {
-	
-	
-	public String checkMood(String msg) {
-		if (msg=="Happy")
+	public String msg;
+	public MoodAnalyzer() {
+		
+	}
+	public MoodAnalyzer(String msg) {
+	this.msg=msg;	
+	}
+	public String checkMood() {
+		if (this.msg=="Happy")
 			return "Happy";
-		else if(msg.contains("Sad"))
+		else if(this.msg.contains("Sad"))
 			return "Sad";
-		else if(msg.contains("Happy"))
+		else if(this.msg.contains("Happy"))
 			return "Happy";
 		else
 			return "Happy";
--- JavaPractice/src/test/java/com/JavaPractice/MoodAnalyzerTest.java
+++ JavaPractice/src/test/java/com/JavaPractice/MoodAnalyzerTest.java
@@ -16,28 +16,28 @@ public class MoodAnalyzerTest {
 	
 	@Test
 	public void WhenGivenHappy_ReturnsHappy() {
-		MoodAnalyzer moodAnalyser=new MoodAnalyzer();
-		String mood=moodAnalyser.checkMood("Happy");
+		MoodAnalyzer moodAnalyser=new MoodAnalyzer("Happy");
+		String mood=moodAnalyser.checkMood();
 		org.junit.Assert.assertEquals("Happy",mood);
 	}
 //Sad
 	@Test
 	public void WhenGivenSad_ReturnsSad() {
-		MoodAnalyzer moodAnalyser=new MoodAnalyzer();
-		String mood=moodAnalyser.checkMood("Sad");
+		MoodAnalyzer moodAnalyser=new MoodAnalyzer("Sad");
+		String mood=moodAnalyser.checkMood();
 		org.junit.Assert.assertEquals("Sad",mood);
 	}
 	@Test
 	public void WhenSadInMsg_ReturnsSad() {
-		MoodAnalyzer moodAnalyser=new MoodAnalyzer();
-		String mood=moodAnalyser.checkMood("Sad");
+		MoodAnalyzer moodAnalyser=new MoodAnalyzer("Sad");
+		String mood=moodAnalyser.checkMood();
 		org.junit.Assert.assertEquals("Sad",mood);
 	}
 	
 	@Test
 	public void WhenGivenAnyMessage_ReturnsHappy() {
-		MoodAnalyzer moodAnalyser=new MoodAnalyzer();
-		String mood=moodAnalyser.checkMood("I am in Any mood !");
+		MoodAnalyzer moodAnalyser=new MoodAnalyzer("I am in Any mood !");
+		String mood=moodAnalyser.checkMood();
 		org.junit.Assert.assertEquals("Happy",mood);
 	}
 	
