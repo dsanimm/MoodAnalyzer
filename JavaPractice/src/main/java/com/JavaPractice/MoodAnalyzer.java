2a9ac47e4040694efa18eb3354bb3085c9507b3d
dsanimm <dsanimm@gmail.com>
2020-09-28 17:36:35 +0530

    UC1

---
+++ JavaPractice/src/main/java/com/JavaPractice/MoodAnalyzer.java
@@ -0,0 +1,12 @@
+package com.JavaPractice;
+
+public class MoodAnalyzer {
+	
+	
+	public String checkMood(String msg) {
+		if (msg=="Happy")
+			return "Happy";
+		else return "Sad";
+	}
+
+}
---
+++ JavaPractice/src/test/java/com/JavaPractice/MoodAnalyzerTest.java
@@ -0,0 +1,30 @@
+package com.JavaPractice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MoodAnalyzerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void WhenGivenHappy_ReturnsHappy() {
+		MoodAnalyzer moodAnalyser=new MoodAnalyzer();
+		String mood=moodAnalyser.checkMood("Happy");
+		org.junit.Assert.assertEquals("Happy",mood);
+	}
+//Sad
+	@Test
+	public void WhenGivenSad_ReturnsSad() {
+		MoodAnalyzer moodAnalyser=new MoodAnalyzer();
+		String mood=moodAnalyser.checkMood("Sad");
+		org.junit.Assert.assertEquals("Sad",mood);
+	}
+}
